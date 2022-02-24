2b2034b9aaa4c045c3a93b7f4c2026c0a884ad80
Yongssun <99157768+Yongssun@users.noreply.github.com>
2022-02-24 16:54:31 +0900

    0224

---
+++ src/chap02/book/B03ByteExample.java
@@ -0,0 +1,17 @@
+package chap02.book;
+
+public class B03ByteExample {
+	public static void main(String[] args) {
+		byte var1 = -128;
+		byte var2 = -30;
+		byte var3 = 0;
+		byte var4 = 127;
+		byte var5 = 30;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+	}
+}
---
+++ src/chap02/book/B08FloatDoubleExample.java
@@ -0,0 +1,27 @@
+package chap02.book;
+
+public class B08FloatDoubleExample {
+	public static void main(String[] args) {
+		double var1 = 3.14;
+		float var2 = 3.14F;
+		
+		double var3 = 0.1234567890123456789;
+		float var4 = 0.1234567890123456789F;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		
+		
+		int var5 = 3000000;
+		double var6= 3e6;
+		float var7 = 3e6F;
+		double var8= 2e-3;
+		
+		System.out.println(var5);
+		System.out.println(var6);
+		System.out.println(var7);
+		System.out.println(var8);
+	}
+}
