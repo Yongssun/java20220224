5ccd3a94f78d453311c2c678a8affbf7054193c1
Yongssun <99157768+Yongssun@users.noreply.github.com>
2022-02-24 12:12:22 +0900

    Initial commit

---
+++ src/chap01/CodingTip.java
@@ -0,0 +1,17 @@
+package chap01;
+
+public class CodingTip {
+	// hint: ctrl+ space
+	public static void main(String[] args) {
+		// 자동 코딩 포맷팅 ctrl + shift+f
+		System.out.println("출력할 텍스트");
+		// 콘솔 출력 코드 (sysout or sout)ctrl+space
+		System.out.println("출력할 텍스트");
+		System.out.println("출력할 텍스트");
+		//실행 ctrl+ f11
+		//주석: ctrl +/
+		//창크기변경 ctrl +m
+		//새파일 ctrl n
+		//저장  ctrl s
+	}
+}
---
+++ src/chap01/Hello.java
@@ -0,0 +1,13 @@
+package chap01;
+/*
+  작성자 : 신용권
+  작성이 : 2015.12.31
+  단축키 crtl+/
+ */
+public class Hello {
+	//플로그램 실행 진입점
+  public static void main(String[] args) {
+	  //콘솔에 출력하는 실행문
+	  System.out.println(" Hello, welcome to the java world!");
+  }//end of main
+}//end of class
